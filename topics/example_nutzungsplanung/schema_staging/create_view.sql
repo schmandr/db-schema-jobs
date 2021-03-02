53def83f95e228cbd0842b03c1608b4b4b112c90
Andreas Schmid <andreas.schmid@bd.so.ch>
2021-03-02 20:05:42 +0100

    Must use single line comments

--- topics/example_nutzungsplanung/schema_staging/create_view.sql
+++ topics/example_nutzungsplanung/schema_staging/create_view.sql
@@ -1,19 +1,18 @@
-/*
-Please don't use any schema names in the view definition; use the ${dbSchema} variable instead.
-Always use CREATE OR REPLACE VIEW, not CREATE VIEW, as the script must be repeatable.
-All view names must end in "_v".
+-- Please don't use any schema names in the view definition; use the ${dbSchema} variable instead.
+-- Always use CREATE OR REPLACE VIEW, not CREATE VIEW, as the script must be repeatable.
+-- All view names must end in "_v".
+--
+-- Example:
+--
+-- CREATE OR REPLACE VIEW ${dbSchema}.t_ili2db_model_v AS
+-- SELECT
+--     filename,
+--     iliversion,
+--     modelname,
+--     importdate
+-- FROM
+--     ${dbSchema}.t_ili2db_model
 
-Example:
-
-CREATE OR REPLACE VIEW ${dbSchema}.t_ili2db_model_v AS
-SELECT
-    filename,
-    iliversion,
-    modelname,
-    importdate
-FROM
-    ${dbSchema}.t_ili2db_model
-*/
 
 CREATE OR REPLACE VIEW ${dbSchema}.t_ili2db_model_v AS
 SELECT
